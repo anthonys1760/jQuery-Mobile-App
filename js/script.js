893bcbf9828c6a13ece1df64ba3a6b71e3923df1
Anthony <anthony.smith1760@gmail.com>
2020-05-13 13:21:34 -0400

    Editing Displays

--- js/script.js
+++ js/script.js
@@ -1,54 +1,75 @@
-$(document).one('pageinit', function(){
+$(document).one('pageinit', function () {
 	// Add Handler
 	$('#submitAdd').on('tap', addRun);
-	
+
+	// Display Runs
+	showRuns();
+
+	/*
+	 * Show all runs on homepage 
+	 */
+	function showRuns() {
+		//get runs object
+		var runs = getRunsObject();
+		// Check if empty
+		if (runs != '' && runs != null) {
+			for (var i = 0; i < runs.length; i++) {
+				$('#stats').append('<li class="ui-body-inherit ui-li-static"><strong>Date:</strong>' + runs[i]["date"] + ' <br><strong>Distance: </strong>' + runs[i]["miles"] + ' <br><strong>Distance: </strong>' +runs[i]["miles"]+'m<div class="controls">' + '<a href="#edit">Edit</a> | <a href="#">Delete</li>');
+			}
+			$('#home').bind('pageinit', function () {
+				$('#stats').listview('refresh');
+			});
+		}
+	}
+
 	/*
 	 * Add a run
 	 */
-	 function addRun(){
+	function addRun() {
 		// Get form values
 		var miles = $('#addMiles').val();
 		var date = $('#addDate').val();
-		
+
 		// Create 'run' object
 		var run = {
 			date: date,
 			miles: parseFloat(miles)
 		};
-		
+
 		var runs = getRunsObject();
-		
+
 		// Add run to runs array
 		runs.push(run);
-		
+
 		alert('Run Added');
-		
+
 		// Set stringified object to localStorage
 		localStorage.setItem('runs', JSON.stringify(runs));
-		
+
 		// Redirect
-		window.location.href="index.html";
-		
+		window.location.href = "index.html";
 		return false;
-	 }
-	 
-	 
-	 /*
+	}
+
+
+	/*
 	 * Get the runs object
 	 */
-	 function getRunsObject(){
+	function getRunsObject() {
 		// Set runs array
 		var runs = new Array();
 		// Get current runs from localStorage
 		var currentRuns = localStorage.getItem('runs');
-		
+
 		// Check localStorage
-		if(currentRuns != null){
+		if (currentRuns != null) {
 			// Set to runs
 			var runs = JSON.parse(currentRuns);
 		}
-		
+
 		// Return runs object
-		return runs.sort(function(a, b){return new Date(b.date) - new Date(a.date)});
-	 }
+		return runs.sort(function (a, b) {
+			return new Date(b.date) - new Date(a.date)
+		});
+	}
 });
